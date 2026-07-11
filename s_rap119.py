3bcae0f33b345e9f409e9a6063646ac3ac01792f
xzhang222 <61070684+xzhang222@users.noreply.github.com>
2026-07-11 18:06:05 -0500

    Add files via upload

--- s_rap119.py
+++ s_rap119.py
@@ -2,6 +2,41 @@ import sys
 import time 
 from tools import *
 
+molecule = 'C78'
+Z = 2
+fast=56
+          #1: 2Fo-Fc dual space recycling
+          #2: sR1 #2223: sR1 in lottery mode #200: bond length guided sR1
+          #3: find fragment orientations  #4: filter orientations
+          #5: locate fragments
+# for sR1
+steps=[10,30,80,156]
+# for bond length guided sR1
+cases=[('C',9,1.39,0.3),('C',152,1.39,0.3),('C',153,1.39,0.3),
+       ('C',154,1.39,0.3),('C',155,1.39,0.3),]
+# for pR1 
+s_angle=20.0 
+max_orientations=10
+free_standing=1  # 1: very first 0: add to a partial structure
+orientation_selected=[0,1] # orientations are labeled as 0, 1, 2...
+orientation_file='orientations_benzenestar.txt'
+
+#fragment0,n_fold=make_molecule('C5.txt')
+#fragment0,n_fold=make_invert_molecule('C7O.txt')
+#fragment0,n_fold=make_fragment_from_mol2('dan3')
+#fragment0,n_fold=make_C6O3()
+#fragment0,n_fold=make_PdCl4()
+#fragment0,n_fold=make_benzene()
+fragment0,n_fold=make_benzenestar()
+#fragment0,n_fold=make_ethynylbenzene()
+#fragment0,n_fold=make_PF6()
+#fragment0,n_fold=make_pentagon()
+#fragment0,n_fold=make_molecule('molecule_C60.txt')
+#fragment0,n_fold=make_molecule('molecule_C7.txt')
+#fragment0,n_fold=make_invert_molecule()
+#fragment0=make_linear('S2.txt')
+#fragment0=make_S2()
+
 starttime=time.time()
 tt='('+time.strftime('%Y-%m-%d, %H:%M:%S',time.localtime())+') '+str(round(time.time()-starttime,1))
 with open(sys.argv[0],'r') as f:
@@ -9,37 +44,16 @@ with open(sys.argv[0],'r') as f:
 with open('history.txt','a') as f:
     print('\n\nstep: \n',tt,file=f)
     print('\n\nprogram '+sys.argv[0]+':\n'+text+'\n\n',file=f)
+if 1:
+    with open('tools.py','r') as f:
+        text=f.read()
+    with open('history.txt','a') as f:
+        print('\n\nprogram '+'tools.py'+':\n'+text+'\n\n',file=f)
 
 print('START calculation...',tt)
 res_file='a.res'
 hkl_file='a.hkl'
 A = matrix_A(res_file)
-
-molecule = 'C78' 
-Z = 2
-fast=2
-          #1: dual space cycling using FFT
-          #2: one cycle of sR1 method
-          #2223: sR1 method in lottery mode
-          #3: find fragment orientations
-          #4: filt orientations
-          #5: locate fragments
-steps=[10,30,80,156]
-cases=[('C','1',153,1.39,0.3),]
-startfrom=1 #r1 hole,min-cycles, gambling: atom to start from                          
-max_runs=1000000  #max dual-space or min cycles
-cutlimit = 4.0  #dual-space: 1.5 probably enough, may choose 2.5
-nextra = 0
-mB = 1 # defaul 1, how much to sharpen F2, 0 no sharpen, 2 fully sharpened, 1 halfway sharpened
-patterson = 0 # 0 start res, 1 patterson, 2 random phase angle             
-
-ntotal_initial= None # num atoms in min, if none will decide automatically 
-s_dual=0.35
-n_refine=3 # num of cycles of refinement
-n_improve = 1 # 1 for 2*Fo-1*Fc
-improve_only = 1 # this flag no longer being used
-extension=False   # recommend False
-double_first=-1  # -1 not to double, 1 to double first run for catching weaker peaks                
 
 if Z<0:
     Z = 1
@@ -51,11 +65,11 @@ if Z<0:
         if done: sys.exit()
 print('When Z=',Z,', density = ',round(density(molecule,Z,A),2))
 
-get_structure_solution(res_file,hkl_file,molecule,Z,ntotal_initial,
-    starttime,s_dual,n_refine,max_runs,improve_only,fast=fast,
-    n_improve=n_improve,cutlimit=cutlimit,extension=extension,
-    double_first=double_first,startfrom=startfrom,nextra=nextra,
-    mB=mB,patterson=patterson,steps=steps,cases=cases)
+get_structure_solution(res_file,hkl_file,molecule,Z,starttime,fast=fast,
+    steps=steps,cases=cases,free_standing=free_standing,
+    orientation_selected=orientation_selected,fragment0=fragment0,
+    n_fold=n_fold,orientation_file=orientation_file,
+    max_orientations=max_orientations,s_angle=s_angle)
 
 with open('history.txt','a') as f:
     print('\n\n\nstep:  \n\n\n',file=f)
